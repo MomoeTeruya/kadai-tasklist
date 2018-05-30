5a59ef82af5a1a748b1b44f5175e53371f85c64c
EC2 Default User <ec2-user@ip-172-31-42-128.us-east-2.compute.internal>
2018-05-30 07:09:24 +0000

    validation

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>KadaiTasklist</title>
+    </head>
+
+    <body>
+        @include('commons.error_messages')
+
+        
+        @yield('content')
+    </body>
+</html>
+
