917b67673df00959d2c32161ed6d18d2cce1ca27
EC2 Default User <ec2-user@ip-172-31-42-128.us-east-2.compute.internal>
2018-06-04 00:52:32 +0000

    Procfile

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="row">
-        <div class="col-xs-12 col-offset-2 col-sm-8 col-offset-4 col-md-8 col-offset-4 col-lg-6 col-offset-6">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
                 <div class="form-group">
